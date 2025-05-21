bf103fe095c7f2babed7c8fba18bf347b6ab026b
Sergey <python_ml_dl@mail.ru>
2025-05-21 21:12:21 +0300

    Еще немного поучился

--- MachineLearn.py
+++ MachineLearn.py
@@ -7,4 +7,5 @@ my_stat = pd.read_csv('https://stepik.org/media/attachments/course/4852/my_stat.
 
 subset_1 = my_stat.iloc[0:10, [0, 2]]
 subset_2 = my_stat.iloc[:, [1, 3]].drop([0, 4], axis=0)
-print(subset_2)
+
+my_stat.rename({'V1': 'session_value', 'V2': 'group', 'V3': 'time', 'V4': 'n_users'}, inplace=True, axis=1)
