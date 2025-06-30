d6e02927c167d7536070935e57d52721aa79ecf8
Sergey <python_ml_dl@mail.ru>
2025-06-30 15:26:17 +0300

    Подготовил реальные данные для тренировки модели. Также решил одну практическую задачу.

--- Real_data_1.py
+++ Real_data_1.py
@@ -10,6 +10,9 @@ submissions_data = pd.read_csv('submissions_data_train.zip')
 events_data['date'] = pd.to_datetime(events_data.timestamp, unit='s')
 events_data['day'] = events_data.date.dt.date
 
+# Добавляем поля date и day в submissions_data
+submissions_data['date'] = pd.to_datetime(submissions_data.timestamp, unit='s')
+submissions_data['day'] = submissions_data.date.dt.date
 # Поиск id Карпова
 def find_carpov_id():
     coca = submissions_data.groupby('user_id', as_index=False).agg({'step_id': 'count'}).sort_values('step_id', ascending=False) # Поиск пользователей, которые решили правильно большинство примеров (counts_of_correct_answers)
@@ -64,13 +67,84 @@ def create_users_data():
     now = 1526772811
     drop_out_threshold = 24 * 60 * 60 * 30
     users_data = (events_data.groupby('user_id', as_index=False).agg({'timestamp': 'max'}) \
-                  .rename(columns={'timestamp': 'last_timestamp'}))
+    .rename(columns={'timestamp': 'last_timestamp'}))
     users_data['is_gone_user'] = (now - users_data.last_timestamp) > drop_out_threshold
     users_data = users_data.merge(users_scores, on='user_id', how='outer').fillna(0)
     users_data = users_data.merge(users_events_data, on='user_id', how='outer')
     users_data = users_data.merge(users_days, on='user_id', how='outer')
     users_data['passed_course'] = users_data.passed > 170
-    print(users_data)
+
+    # Вычисляем первый раз, когда пользователь зашел на курс
+    user_min_time = events_data.groupby('user_id', as_index=False) \
+        .agg({'timestamp': 'min'}) \
+        .rename({'timestamp': 'min_timestamp'}, axis='columns')
+    users_data = users_data.merge(user_min_time, on='user_id', how='outer')
+
+    # Не надо так делать, это очень долго
+    # events_data_train = pd.DataFrame()
+    # for user_id in users_data.user_id:
+    #     min_user_time = users_data[users_data['user_id'] == user_id]['min_timestamp'].item()
+    #     time_threshold = min_user_time + 3 * 24 * 60 * 60
+    #     user_events_data = events_data[(events_data['user_id'] == user_id) & (events_data['timestamp'] < time_threshold)]
+    #     event_data_train = pd.concat([event_data_train, user_events_data])
+
+    # Количество секунд в трёх днях
+    learning_time_threshold = 3 * 24 * 60 * 60
+
+    # Находим все действия, которые пользователь сделал за 3 дня, после своего начала курса
+    events_data_train = events_data.merge(user_min_time, on='user_id', how='outer') \
+    .query('(timestamp - min_timestamp) <= @learning_time_threshold')
+
+    # Проверяем найденные значение на соответствие условию поиска
+    # print(events_data_train.groupby('user_id').day.nunique().max())
+
+    # Повторяем все действия с submissions_data
+    submissions_data_train = submissions_data.merge(user_min_time, on='user_id', how='outer') \
+    .query('(timestamp - min_timestamp) <= @learning_time_threshold')
+    # print(submissions_data_train.groupby('user_id').day.nunique().max())
+
+    # Готовим данные для обучения модели
+    X = submissions_data_train.groupby('user_id') \
+        .day.nunique().to_frame().reset_index() \
+        .rename(columns={'day': 'days'})
+    steps_tried = submissions_data_train.groupby('user_id') \
+        .step_id.nunique().to_frame().reset_index() \
+        .rename(columns={'step_id': 'steps_tried'})
+    X = X.merge(steps_tried, on='user_id', how='outer')
+    X = X.merge(submissions_data_train.pivot_table(index='user_id',
+                                                   columns='submission_status',
+                                                   values='step_id',
+                                                   aggfunc='count',
+                                                   fill_value=0).reset_index())
+    X['correct_ratio'] = X.correct / (X.correct + X.wrong)
+    X = X.merge(events_data_train.pivot_table(index='user_id',
+                                              columns='action',
+                                              values='step_id',
+                                              aggfunc='count',
+                                              fill_value=0).reset_index()[['user_id', 'viewed']],
+                on='user_id',
+                how='outer'
+                )
+    X = X.fillna(0)
+    X = X.merge(users_data[['user_id', 'passed_course', 'is_gone_user']],
+                on='user_id',
+                how='outer')
+    X = X[X.is_gone_user | X.passed_course]
+    y = X['passed_course'].map(int)
+    X = X.drop(['passed_course', 'is_gone_user'], axis='columns')
+    X = X.set_index(X.user_id)
+    X = X.drop('user_id', axis='columns')
+
+    return (X, y)
+
+#   Поиск шага, которого большинство завершило курс
+def you_shall_not_pass():
+    users_last_time = submissions_data.groupby('user_id', as_index=False) \
+        .agg({'timestamp': 'max'}) \
+        .rename({'timestamp': 'last_timestamp'}, axis='columns')
+    print(submissions_data.merge(users_last_time, on='user_id', how='outer') \
+        .query('(timestamp == last_timestamp) & (submission_status == "wrong")') \
+        ['step_id'].value_counts())
 
 if __name__ == '__main__':
     create_users_data()
